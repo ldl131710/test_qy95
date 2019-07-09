da456724f55caa8351b62afe1f6fc4c46fd971b5
ldl <ldl131710@163.comm>
2019-07-09 15:08:33 +0800

    部门管理实体类增加地址的属性

--- src/main/java/aaa/test_git2/entity/Dept.java
+++ src/main/java/aaa/test_git2/entity/Dept.java
@@ -7,6 +7,9 @@ public class Dept {
     private Long deptno;
     private String dname;
     private String loc;
+    private String address;
+    private String age;
+    
 
     public Long getDeptno() {
         return deptno;
