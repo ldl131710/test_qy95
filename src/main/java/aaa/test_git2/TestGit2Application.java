a51673bd5c63b7a290f4a5a5c431d684c9cc3111
ldl <ldl131710@163.comm>
2019-07-09 14:23:18 +0800

    框架的搭建

---
+++ src/main/java/aaa/test_git2/TestGit2Application.java
@@ -0,0 +1,13 @@
+package aaa.test_git2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestGit2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestGit2Application.class, args);
+    }
+
+}
---
+++ src/test/java/aaa/test_git2/TestGit2ApplicationTests.java
@@ -0,0 +1,16 @@
+package aaa.test_git2;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TestGit2ApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
