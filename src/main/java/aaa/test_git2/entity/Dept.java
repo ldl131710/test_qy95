c172168f43219835f41371ed5603233527104ac6
ldl <ldl131710@163.comm>
2019-07-09 14:56:01 +0800

    声明了一个实体类

---
+++ src/main/java/aaa/test_git2/entity/Dept.java
@@ -0,0 +1,34 @@
+package aaa.test_git2.entity;
+
+/**
+ * 部门管理实体类
+ */
+public class Dept {
+    private Long deptno;
+    private String dname;
+    private String loc;
+
+    public Long getDeptno() {
+        return deptno;
+    }
+
+    public void setDeptno(Long deptno) {
+        this.deptno = deptno;
+    }
+
+    public String getDname() {
+        return dname;
+    }
+
+    public void setDname(String dname) {
+        this.dname = dname;
+    }
+
+    public String getLoc() {
+        return loc;
+    }
+
+    public void setLoc(String loc) {
+        this.loc = loc;
+    }
+}
